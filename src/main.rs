f73f5f3a87802dd434a96ad9bc7fddb6e9c2ec59
Berkus Decker <berkus+github@metta.systems>
2024-10-08 11:09:56 +0300

    refactor: 📦 Update code

--- src/main.rs
+++ src/main.rs
@@ -14,85 +14,90 @@
 //
 // Author: zadig <thomas chr(0x40) bailleux.me>
 
-extern crate ole;
-extern crate ansi_term;
-extern crate clap;
-
-mod olekit;
 mod format;
+mod olekit;
 
 fn main() {
-  let matches = clap::App::new("OLEkit")
-    .version(env!("CARGO_PKG_VERSION"))
-    .about(env!("CARGO_PKG_DESCRIPTION"))
-    .subcommand(
-      clap::SubCommand::with_name("ls")
-        .about("List OLE file entries")
-        .arg(clap::Arg::with_name("FILE")
-          .help("OLE file to analyze")
-          .required(true)
-          .index(1)
+    let matches = clap::App::new("OLEkit")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            clap::SubCommand::with_name("ls")
+                .about("List OLE file entries")
+                .arg(
+                    clap::Arg::with_name("FILE")
+                        .help("OLE file to analyze")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    clap::Arg::with_name("human")
+                        .short("h")
+                        .long("human-readable")
+                        .required(false)
+                        .takes_value(false)
+                        .help("with -l and -s, print sizes like 1K 234M 2G etc."),
+                )
+                .arg(
+                    clap::Arg::with_name("size")
+                        .short("s")
+                        .long("size")
+                        .required(false)
+                        .takes_value(false)
+                        .help("print the allocated size of each file, in blocks"),
+                )
+                .arg(
+                    clap::Arg::with_name("color")
+                        .long("color")
+                        .required(false)
+                        .takes_value(false)
+                        .help("colorize the output"),
+                )
+                .arg(
+                    clap::Arg::with_name("details")
+                        .short("d")
+                        .long("details")
+                        .required(false)
+                        .takes_value(false)
+                        .help("print more details for each entry"),
+                )
+                .arg(
+                    clap::Arg::with_name("idirid")
+                        .short("i")
+                        .long("idirid")
+                        .required(false)
+                        .takes_value(false)
+                        .help("print the index number of each file"),
+                )
+                .arg(
+                    clap::Arg::with_name("full-path")
+                        .short("f")
+                        .long("full-path")
+                        .required(false)
+                        .takes_value(false)
+                        .help("print the full path of each entry"),
+                ),
         )
-        .arg(clap::Arg::with_name("human")
-          .short("h")
-          .long("human-readable")
-          .required(false)
-          .takes_value(false)
-          .help("with -l and -s, print sizes like 1K 234M 2G etc.")
+        .subcommand(
+            clap::SubCommand::with_name("cat")
+                .about("Concatenate entries and print on the standard output")
+                .arg(
+                    clap::Arg::with_name("FILE")
+                        .help("OLE file to analyze")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    clap::Arg::with_name("ID")
+                        .help("IDs of the entries")
+                        .required(true)
+                        .index(2)
+                        .min_values(1),
+                ),
         )
-        .arg(clap::Arg::with_name("size")
-          .short("s")
-          .long("size")
-          .required(false)
-          .takes_value(false)
-          .help("print the allocated size of each file, in blocks")
-        )
-        .arg(clap::Arg::with_name("color")
-          .long("color")
-          .required(false)
-          .takes_value(false)
-          .help("colorize the output")
-        )
-        .arg(clap::Arg::with_name("details")
-          .short("d")
-          .long("details")
-          .required(false)
-          .takes_value(false)
-          .help("print more details for each entry")
-        )
-        .arg(clap::Arg::with_name("idirid")
-          .short("i")
-          .long("idirid")
-          .required(false)
-          .takes_value(false)
-          .help("print the index number of each file")
-        )
-        .arg(clap::Arg::with_name("full-path")
-          .short("f")
-          .long("full-path")
-          .required(false)
-          .takes_value(false)
-          .help("print the full path of each entry")
-        )
-    )
-    .subcommand(
-      clap::SubCommand::with_name("cat")
-        .about("Concatenate entries and print on the standard output")
-        .arg(clap::Arg::with_name("FILE")
-          .help("OLE file to analyze")
-          .required(true)
-          .index(1)
-        )
-        .arg(clap::Arg::with_name("ID")
-          .help("IDs of the entries")
-          .required(true)
-          .index(2)
-          .min_values(1)
-        )
-    )
-    .get_matches();
-  match olekit::olekit(&matches) {
-    Err(e) => eprintln!("An error has occured: {}", e),
-    _ => {}
-  };
+        .get_matches();
+    match olekit::olekit(&matches) {
+        Err(e) => eprintln!("An error has occured: {}", e),
+        _ => {}
+    };
 }
--- src/olekit.rs
+++ src/olekit.rs
@@ -14,68 +14,66 @@
 //
 // Author: zadig <thomas chr(0x40) bailleux.me>
 
-use std;
 use clap;
 use ole;
+use std;
 
+pub(crate) fn olekit(
+    all_matches: &clap::ArgMatches,
+) -> Result<(), std::boxed::Box<dyn std::error::Error>> {
+    // ls
+    if let Some(matches) = all_matches.subcommand_matches("ls") {
+        let file = matches.value_of("FILE").unwrap();
+        let parser = ole::Reader::from_path(file)?;
+        let human = matches.is_present("human");
+        let size = matches.is_present("size");
+        let color = matches.is_present("color");
+        let more_details = matches.is_present("details");
+        let idirid = matches.is_present("idirid");
+        let full_path = matches.is_present("full-path");
+        let formatter: Box<dyn super::format::Formatter>;
 
-pub(crate) fn olekit(all_matches: &clap::ArgMatches)
-      -> Result<(), std::boxed::Box<dyn std::error::Error>> {
+        formatter = Box::new(super::format::DetailsFormatter {
+            human: human,
+            size: size,
+            color: color,
+            more_details: more_details,
+            idirid: idirid,
+            full_path: full_path,
+        });
 
-  // ls
-  if let Some(matches) = all_matches.subcommand_matches("ls") {
-    let file = matches.value_of("FILE").unwrap();
-    let parser = ole::Reader::from_path(file)?;
-    let human = matches.is_present("human");
-    let size = matches.is_present("size");
-    let color = matches.is_present("color");
-    let more_details = matches.is_present("details");
-    let idirid = matches.is_present("idirid");
-    let full_path = matches.is_present("full-path");
-    let formatter: Box<dyn super::format::Formatter>;
-
-    formatter = Box::new(super::format::DetailsFormatter {
-        human: human,
-        size: size,
-        color: color,
-        more_details: more_details,
-        idirid: idirid,
-        full_path: full_path
-      });
-
-    formatter.print_entries(parser.iterate().collect());
-  }
-  // cat
-  else if let Some(matches) = all_matches.subcommand_matches("cat") {
-    let file = matches.value_of("FILE").unwrap();
-    let parser = ole::Reader::from_path(file)?;
-    let entries: std::vec::Vec<&ole::Entry> = parser.iterate().collect();
-    let files: std::vec::Vec<&str>
-        = matches.values_of("ID").unwrap().collect();
-    for sid in files {
-      if let Ok(id) = sid.parse::<usize>() {
-        if id < entries.len() {
-          let entry = entries[id];
-          let mut buf = std::vec::Vec::<u8>::new();
-          if let Ok(mut slice) = parser.get_entry_slice(entry) {
-            use std::io::{Read, Write};
-            match slice.read_to_end(&mut buf) {
-              Ok(_n) => {
-                std::io::stdout().write_all(&buf)?;
-                std::io::stdout().flush()?;
-              },
-              Err(e) => eprintln!("Can't print entry {}: {}", id, e)
-            }
-          } else {
-            eprintln!("Can't print entry {}.", id);
-          }
-        } else {
-          eprintln!("Entry {} doesn't exist", id);
-        }
-      } else {
-        eprintln!("Invalid entry id: {}", sid);
-      }
+        formatter.print_entries(parser.iterate().collect());
     }
-  }
-  Ok(())
+    // cat
+    else if let Some(matches) = all_matches.subcommand_matches("cat") {
+        let file = matches.value_of("FILE").unwrap();
+        let parser = ole::Reader::from_path(file)?;
+        let entries: std::vec::Vec<&ole::Entry> = parser.iterate().collect();
+        let files: std::vec::Vec<&str> = matches.values_of("ID").unwrap().collect();
+        for sid in files {
+            if let Ok(id) = sid.parse::<usize>() {
+                if id < entries.len() {
+                    let entry = entries[id];
+                    let mut buf = std::vec::Vec::<u8>::new();
+                    if let Ok(mut slice) = parser.get_entry_slice(entry) {
+                        use std::io::{Read, Write};
+                        match slice.read_to_end(&mut buf) {
+                            Ok(_n) => {
+                                std::io::stdout().write_all(&buf)?;
+                                std::io::stdout().flush()?;
+                            }
+                            Err(e) => eprintln!("Can't print entry {}: {}", id, e),
+                        }
+                    } else {
+                        eprintln!("Can't print entry {}.", id);
+                    }
+                } else {
+                    eprintln!("Entry {} doesn't exist", id);
+                }
+            } else {
+                eprintln!("Invalid entry id: {}", sid);
+            }
+        }
+    }
+    Ok(())
 }
